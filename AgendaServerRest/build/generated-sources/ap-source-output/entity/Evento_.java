1b207e57c9a69780390723b827aa2c0400cdc220
Ángela Martina Padrón Pol <apadronpol@gmail.com>
2017-12-21 19:39:44 +0100

    Angela Branch -> diseño + todo ok :)

--- AgendaServerRest/build/generated-sources/ap-source-output/entity/Evento_.java
+++ AgendaServerRest/build/generated-sources/ap-source-output/entity/Evento_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-21T17:05:01")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-21T19:36:08")
 @StaticMetamodel(Evento.class)
 public class Evento_ { 
 
